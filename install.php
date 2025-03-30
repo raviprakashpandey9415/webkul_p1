301815e3f011f4901a090ac72ac2387e1b08bf1e
raviprakashpandey9415 <raviprakashpandey9415@gmail.com>
2025-03-30 20:14:00 +0530

    Delete install.php

--- install.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once("./backend/database/connection.php");
-
-$connection = new connection();
-$connection->connectToDB();
-$connection->makeTables();
-header("Location: http://localhost/Social%20Media/");
